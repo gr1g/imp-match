8c030efd6409d488be4730cdddc11b898dadac8c
gr1g <guillaume.haeringer@gmail.com>
2015-05-07 18:02:53 -0400

    Bug corrected

--- Impossible_matches/dummy.h
+++ Impossible_matches/dummy.h
@@ -20,78 +20,82 @@ using namespace std;
 
 
 class DUMMY {
-	
+    
 public:
-	
-	DUMMY();
-	
-	ofstream output;
-	
-	// Main Steps
-	void loadData(string dataFile); // loads data from dataFile
-	void stepOne(); // main algorithm
-	void stepTwo(); // refinement with stars
-	void postAnalysis();
-	void printResults(string outputFile);
-	
-	// Main functions
-	
-	int rankedOnce();
-	int rankedMutiple();
-	
-	int newrankedOnce(vector<vector<int> > &rankingsOfPositions);
-	int newrankedMutiple(vector<vector<int> > &rankingsOfPositions);
-	
-	void simpleBlocks();    // calls in a loop rankedOnce() and RankedMultiple(): to eliminate easy cases of impossible matches (see web appendix)
-	void petitsblocks(vector<vector<int> > &rankingsOfPositions);
-	vector<vector<int> > simplify_i0(vector<vector<int> > profile, int i0, int s0); // construct rankings from profile
-	
-	void comprehensiveMatching(vector<vector<int> > &rankings, vector<int> &candidates, vector<int> &matching_candidate, vector<int> &matching_department); // construct a comprehensive matching
-	void checkComprehensiveness(vector<vector<int> > rankings, vector<int> candidates, string step);
-	void checkImpossible(vector<vector<int> > profile, int i0, int s0, vector<vector<int> > &impossible); // main function to check a candidate is impossible
-
-	vector<int> buildPossible_K(vector<int> Gamma_cand, vector<vector<int> > rankings, vector<int> J_0);
-	void buildGamma(vector<vector<int> > rankings, vector<int> candidates, int i0, vector<int> matching_candidate, vector<int> matching_department, vector<int> &J_0, vector<int> &Gamma_dep, vector<int> &Gamma_cand); // construct the set Gamma_cand
-	vector<vector<int> > admissibleGraph(vector<vector<int> > rankings,vector<int> candidates, vector<int> matchCandidates); // to get a comprehensive matching
-	vector<vector<int> > graphOfJ(vector<vector<int> > rankings, vector<int> candidates, vector<int> Gamma_cand, vector<int> Gamma_dep, vector<int> K);    // construct the edge set used to compute a maximum matching (rankings restricted to Gamma_cand, truncated at K)
-	
-	void matchingStars(vector<vector<int> > profile, int dim);   // assign candidates ranked only 1st (and at least twice) to a position.
-	void getStars(vector<vector<int> > profile, vector<int> &theStars, vector<vector<int> > &theirChoices);
-	vector<vector<int> > implementStarsChoices(vector<int> chosenPositions, vector<vector<int> > choiceSet, vector<vector<int> > profile);
-	
-	void nextChoices(vector<int> &theSet, vector<int> &capacity);
-
-	int marketCleared(vector<vector<int> > &profile);
-	
-	void copyRankings(vector<vector<int> > &source, vector<vector<int> > &copy);
-	
-	int loopK(vector<int> K, vector<vector<int> > rankings, vector<int> candidates, vector<int> Gamma_cand, vector<int> Gamma_dep, vector<int> J_0, int s0, int i0);
-	
-	void notImpossibles(vector<int> candidate, vector<vector<int> > rankings, vector<int> matching_candidate, vector<vector<int> > &impossibles);
-	
-	
-	// Maximum matching functions
-	int dfs(vector<vector<int> > rankings, int a, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department, vector<int> &visited);
-	int dfsExp(vector<vector<int> > rankings, int a, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department);
-	int bipMatch(vector<vector<int> > rankings, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department);
-	
-	
-	// Additional functions
-
-	int index(const vector<int> &S, int a); // Get the index of "a" in a vector "S" (spans all coordinate starting from 0)
-	int indexInRankings(const vector<int> &S, int a);   // Get the index of "candidate" a in a ranking "S" (spans all coordinate starting from 1)
-	void Truncate(vector<int> &S, int a);
-	bool presentInVector(const vector<int> &S, int z);  // Return 1 if "z" is in "S", 0 otherwise
-	signed long int coeff(int n, int k); // Compute # of combinations for a set of size k in a set of size n.
-	void next_subset(vector<int> &selectedIndex, int n, int k); // construct the next subset K.
-
-	// Variables, vectors, ...
-	string theFunction;
-	int maxIter;
-	vector<vector<int> > originalRankings; // The data for each year
-	vector<vector<int> > originalImpossible; // Recording who is an impossible match. Same dimensions are originalRankings
-	vector<vector<int> > hired;
-
+    
+    DUMMY();
+    
+    ofstream output;
+    
+    // Main Steps
+    void loadData(string dataFile); // loads data from dataFile
+    void stepOne(); // main algorithm
+    void stepTwo(); // refinement with stars
+    void postAnalysis();
+    void printResults(string outputFile);
+    
+    // Main functions
+    
+    int rankedOnce();
+    int rankedMutiple();
+    
+    int newrankedOnce(vector<vector<int> > &rankingsOfPositions);
+    int newrankedMutiple(vector<vector<int> > &rankingsOfPositions);
+    
+    void simpleBlocks();    // calls in a loop rankedOnce() and RankedMultiple(): to eliminate easy cases of impossible matches (see web appendix)
+    void petitsblocks(vector<vector<int> > &rankingsOfPositions);
+    vector<vector<int> > simplify_i0(vector<vector<int> > profile, int i0, int s0); // construct rankings from profile
+    
+    void comprehensiveMatching(vector<vector<int> > &rankings, vector<int> &candidates, vector<int> &matching_candidate, vector<int> &matching_department); // construct a comprehensive matching
+    void checkComprehensiveness(vector<vector<int> > rankings, vector<int> candidates, string step);
+    void checkImpossible(vector<vector<int> > profile, int i0, int s0, vector<vector<int> > &impossible); // main function to check a candidate is impossible
+    
+    vector<int> buildPossible_K(vector<int> Gamma_cand, vector<vector<int> > rankings, vector<int> J_0);
+    void buildGamma(vector<vector<int> > rankings, vector<int> candidates, int i0, vector<int> matching_candidate, vector<int> matching_department, vector<int> &J_0, vector<int> &Gamma_dep, vector<int> &Gamma_cand); // construct the set Gamma_cand
+    vector<vector<int> > admissibleGraph(vector<vector<int> > rankings,vector<int> candidates, vector<int> matchCandidates); // to get a comprehensive matching
+    vector<vector<int> > graphOfJ(vector<vector<int> > rankings, vector<int> candidates, vector<int> Gamma_cand, vector<int> Gamma_dep, vector<int> K);    // construct the edge set used to compute a maximum matching (rankings restricted to Gamma_cand, truncated at K)
+    
+    void matchingStars(vector<vector<int> > profile, int dim);   // assign candidates ranked only 1st (and at least twice) to a position.
+    void getStars(vector<vector<int> > profile, vector<int> &theStars, vector<vector<int> > &theirChoices);
+    vector<vector<int> > implementStarsChoices(vector<int> chosenPositions, vector<vector<int> > choiceSet, vector<vector<int> > profile);
+    
+    void nextChoices(vector<int> &theSet, vector<int> &capacity);
+    
+    int marketCleared(vector<vector<int> > &profile);
+    
+    void copyRankings(vector<vector<int> > &source, vector<vector<int> > &copy);
+    
+    int loopK(vector<int> K, vector<vector<int> > rankings, vector<int> candidates, vector<int> Gamma_cand, vector<int> Gamma_dep, vector<int> J_0, int s0, int i0);
+    
+    void notImpossibles(vector<int> candidate, vector<vector<int> > rankings, vector<int> matching_candidate, vector<vector<int> > &impossibles);
+    
+    
+    // Maximum matching functions
+    int dfs(vector<vector<int> > rankings, int a, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department, vector<int> &visited);
+    int dfsExp(vector<vector<int> > rankings, int a, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department);
+    int bipMatch(vector<vector<int> > rankings, vector<int> candidates, vector<vector<int> > edge, vector<int> &matching_candidate, vector<int> &matching_department);
+    
+    
+    // Additional functions
+    
+    int index(const vector<int> &S, int a); // Get the index of "a" in a vector "S" (spans all coordinate starting from 0)
+    int indexInRankings(const vector<int> &S, int a);   // Get the index of "candidate" a in a ranking "S" (spans all coordinate starting from 1)
+    void Truncate(vector<int> &S, int a);
+    bool presentInVector(const vector<int> &S, int z);  // Return 1 if "z" is in "S", 0 otherwise
+    signed long int coeff(int n, int k); // Compute # of combinations for a set of size k in a set of size n.
+    void next_subset(vector<int> &selectedIndex, int n, int k); // construct the next subset K.
+    
+    // Variables, vectors, ...
+    string theFunction;
+    int maxIter;
+    vector<vector<int> > originalRankings; // The data for each year
+    vector<vector<int> > originalImpossible; // Recording who is an impossible match. Same dimensions are originalRankings
+    vector<vector<int> > hired;
+    
+    
+    int numberPositionsCleared;
+    int totalNumberCombinations;
+    
 };
 
 #endif /* defined(__Impossible_matches__dummy__) */
@@ -111,4 +115,3 @@ public:
 //	vector<int> J_0;    // Set J_0 (see paper)
 //	vector<int> Gamma_cand; // The set that is (or not) a block ($\mathbf{J}$ in the proof, see paper)
 //	vector<int> Gamma_dep; // Acceptable positions for candidates in Gamma_cand
-
--- Impossible_matches/main.cpp
+++ Impossible_matches/main.cpp
@@ -22,112 +22,111 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
-	
-	
-	int year_index;
-	string filename;
-	time_t t1,t2;    // time variables to count the time spent running the program
-	string second, minute, hour;
-	long hours,minutes,seconds;
-	second = "second";
-	minute = "minute";
-	hour = "hour";
-	
-	ofstream output;
-	
-	
-	
-	for (year_index = 3 ; year_index < 12 ; year_index++) {
-		if (year_index == 0) {
-			filename = "1999-data.txt";
-		}
-		if (year_index == 1) {
-			filename = "2000-data.txt";
-		}
-		if (year_index == 2) {
-			filename = "2001-data.txt";
-		}
-		if (year_index == 3) {
-			filename = "2002-data.txt";
-		}
-		if (year_index == 4) {
-			filename = "2003-data.txt";
-		}
-		if (year_index == 5) {
-			filename = "2004-data.txt";
-		}
-		if (year_index == 6) {
-			filename = "2005-data.txt";
-		}
-		if (year_index == 7) {
-			filename = "2006-data.txt";
-		}
-		if (year_index == 8) {
-			filename = "2007-data.txt";
-		}
-		if (year_index == 9) {
-			filename = "2008-data.txt";
-		}
-		if (year_index == 10) {
-			filename = "2009-data.txt";
-		}
-		if (year_index == 11) {
-			filename = "2010-data.txt";
-		}
-		
-		output.open ("result.txt", std::ofstream::out | std::ofstream::app);
-		
-		cout << "\n\n";
-		cout << "*************************************\n*\n*";
-		cout << "\tYear " << year_index+1999 << "\n*\n*";
-		cout << "*************************************\n\n";
-		
-		output << "\n\n";
-		output << "*************************************\n*\n*";
-		output << "\tYear " << year_index+1999 << "\n*\n*";
-		output << "*************************************\n\n";
-		
-		
-		output.close();
-		
-		
-		(void) time(&t1);
-		
-		
-		
-		
-		DUMMY dummy;
-		dummy.loadData(filename.c_str());
-		dummy.stepOne();
-		//dummy.stepTwo();
-		dummy.postAnalysis();
-		
-		
-		(void) time(&t2);
-		hours = (t2-t1)/3600;
-		minutes = (t2-t1)/60-60*hours;
-		seconds = t2-t1-60*minutes-3600*hours;
-		if (minutes > 1 ) {
-			minute = "minutes";
-		}
-		if (seconds > 1 ) {
-			second = "seconds";
-		}
-		if (hours > 1 ) {
-			hour = "hours";
-		}
-		cout << "\nTime spent: ";
-		if (hours < 1) {
-			if (minutes < 1 ) {
-				cout << seconds << " " << second << endl;
-			} else {
-				cout << minutes << " " << minute << ", " << seconds << " " << second <<endl;
-			}
-		} else {
-			cout << hours << " " << hour << ", " << minutes << " " << minute << ", " << seconds << " " << second <<endl;
-		}
-	}
-	
-	
+    
+    
+    int year_index;
+    string filename;
+    time_t t1,t2;    // time variables to count the time spent running the program
+    string second, minute, hour;
+    long hours,minutes,seconds;
+    second = "second";
+    minute = "minute";
+    hour = "hour";
+    
+    ofstream output;
+    
+    
+    
+    for (year_index = 5 ; year_index < 12 ; year_index++) {
+        if (year_index == 0) {
+            filename = "1999-data.txt";
+        }
+        if (year_index == 1) {
+            filename = "2000-data.txt";
+        }
+        if (year_index == 2) {
+            filename = "2001-data.txt";
+        }
+        if (year_index == 3) {
+            filename = "2002-data.txt";
+        }
+        if (year_index == 4) {
+            filename = "2003-data.txt";
+        }
+        if (year_index == 5) {
+            filename = "2004-data.txt";
+        }
+        if (year_index == 6) {
+            filename = "2005-data.txt";
+        }
+        if (year_index == 7) {
+            filename = "2006-data.txt";
+        }
+        if (year_index == 8) {
+            filename = "2007-data.txt";
+        }
+        if (year_index == 9) {
+            filename = "2008-data.txt";
+        }
+        if (year_index == 10) {
+            filename = "2009-data.txt";
+        }
+        if (year_index == 11) {
+            filename = "2010-data.txt";
+        }
+        
+        output.open ("result.txt", std::ofstream::out | std::ofstream::app);
+        
+        cout << "\n\n";
+        cout << "*************************************\n*\n*";
+        cout << "\tYear " << year_index+1999 << "\n*\n*";
+        cout << "*************************************\n\n";
+        
+        output << "\n\n";
+        output << "*************************************\n*\n*";
+        output << "\tYear " << year_index+1999 << "\n*\n*";
+        output << "*************************************\n\n";
+        
+        
+        output.close();
+        
+        
+        (void) time(&t1);
+        
+        
+        
+        
+        DUMMY dummy;
+        dummy.loadData(filename.c_str());
+        dummy.stepOne();
+        //dummy.stepTwo();
+        dummy.postAnalysis();
+        
+        
+        (void) time(&t2);
+        hours = (t2-t1)/3600;
+        minutes = (t2-t1)/60-60*hours;
+        seconds = t2-t1-60*minutes-3600*hours;
+        if (minutes > 1 ) {
+            minute = "minutes";
+        }
+        if (seconds > 1 ) {
+            second = "seconds";
+        }
+        if (hours > 1 ) {
+            hour = "hours";
+        }
+        cout << "\nTime spent: ";
+        if (hours < 1) {
+            if (minutes < 1 ) {
+                cout << seconds << " " << second << endl;
+            } else {
+                cout << minutes << " " << minute << ", " << seconds << " " << second <<endl;
+            }
+        } else {
+            cout << hours << " " << hour << ", " << minutes << " " << minute << ", " << seconds << " " << second <<endl;
+        }
+    }
+    
+    
 }
-
